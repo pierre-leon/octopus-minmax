286fdec9493d78b8c22e949b61e98889fc97a64f
Joe Shufflebotham <joe.shufflebotham13@gmail.com>
2025-02-01 18:18:05 +0000

    docker configs and code changes to support

--- config.py
+++ config.py
@@ -1,8 +1,1 @@
-# Add your stuff here
-API_KEY = ""
-# Your Octopus Energy account number. Starts with A-
-ACC_NUMBER = ""
 BASE_URL = "https://api.octopus.energy/v1"
-DISCORD_WEBHOOK =""
-OCTOPUS_LOGIN_EMAIL = ""
-OCTOPUS_LOGIN_PASSWD = ""
