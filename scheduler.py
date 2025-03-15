158772054bb9ebbb88a73f225717257d74d365f5
Ishay Rosenthal <we@rehov.net>
2025-03-15 22:20:46 +0000

    Add bot version to notification

--- scheduler.py
+++ scheduler.py
@@ -8,7 +8,7 @@ from main import run_tariff_compare, send_notification
 last_execution_date = None
 
 if config.ONE_OFF_RUN:
-    send_notification(message="Octobot on. Running a one off comparison.")
+    send_notification(message=f"Octobot {config.BOT_VERSION} on. Running a one off comparison.")
     run_tariff_compare()
 else:
     send_notification(message=f"Welcome to Octopus MinMax Bot. I will run your comparisons at {config.EXECUTION_TIME}")
@@ -22,7 +22,7 @@ else:
             last_execution_date = current_date
             # 10 Sec - 10 Min Random Delay to prevent all users attempting to access API at same time
             delay = random.randint(10,600) 
-            send_notification(message=" Octobot on. Initiating comparison in {:.1f} minutes".format(delay/60))
+            send_notification(message=f"Octobot {config.BOT_VERSION} on. Initiating comparison in {delay/60:.1f} minutes")
             delay = time.sleep(delay)
             run_tariff_compare()
 
